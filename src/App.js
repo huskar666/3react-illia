94596c1b58d6b20b2a69ab55720faf35f1611235
huskar666 <148646923+huskar666@users.noreply.github.com>
2025-08-30 23:10:23 +0300

    code

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Profile from "./components/Profile";
+import user from "./data/user.json";
+
+export default function App() {
+
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+}
